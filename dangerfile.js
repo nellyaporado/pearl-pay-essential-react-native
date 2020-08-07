5adb09ba7ded2dedbca4d390a061a225379f3731
nellyaporado <nelly@pearlpay.io>
2020-08-07 14:04:34 +0800

    testing dangersj

--- dangerfile.js
+++ dangerfile.js
@@ -12,48 +12,7 @@ if (packageChanged) {
     warn('Changes were made to package.json');
 }
 
-// Warns if file contains 'console.log'
-const consoleLogsInFiles = async () => {
-    const diffs = await Promise.all(
-        danger.git.modified_files.map(async filePath => ({
-            filePath,
-            diff: await danger.git.diffForFile(filePath),
-        })),
-    );
-    const foundConsoleLogs: {
-        filePath: string,
-        count: number,
-    }[] = [];
-    diffs.forEach(({diff, filePath}) => {
-        if (diff !== null) {
-            // match all 'console.log' strings, case insensitive
-            const consoleLogCount = diff.added.match(/console\.log\(([^)]+)\);/igm);
-            if (consoleLogCount !== null) {
-                foundConsoleLogs.push({
-                    filePath,
-                    count: consoleLogCount.length,
-                });
-            }
-        }
-    });
-
-    if (foundConsoleLogs.length > 0) {
-        warn(
-            `<p>Found <b>'console.log'</b> in following files: </p>
-      <ul> 
-      ${foundConsoleLogs
-          .filter(({filePath}) => filePath !== 'dangerfile.ts')
-          .map(
-              ({filePath, count}) =>
-                  `<li><code>${filePath}</code> - count: <code>${count}</code></li>`,
-          )} 
-      </ul>
-      <p>Please remove if they are already not necessary.</p>
-      `,
-        );
-    }
-};
-
-(async () => {
-    await consoleLogsInFiles();
-  })();
+// No assignee assigned
+if (danger.github.pr.assignee === null) {
+    warn('Please assign at least 1 assignee.');
+}
